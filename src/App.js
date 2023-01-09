3a1b7cf6b85f55976f9083fdd47a935a7183cbee
Abhishek5101 <miabhishekkulkarni@gmail.com>
2023-01-09 12:50:55 +0530

    add code from Codesandbox

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import "./styles.css";
+import React, { useState } from "react";
+
+const emojiDictionary = {
+  "🇨🇭": "Switzerland",
+  "🇰🇷": "South Korea",
+  "🇱🇰": "Sri Lanka",
+  "🇮🇳": "India",
+  "🇬🇷": "Greece",
+  "🇳🇬": "Nigeria",
+  "🇧🇹": "Bhutan",
+  "🇪🇬": "Egypt",
+  "🇸🇬": "Singapore",
+  "🇪🇪": "Estonia"
+};
+
+const emojis = Object.keys(emojiDictionary);
+
+export default function App() {
+  const [emoji, setEmoji] = useState("");
+  const [meaning, setMeaning] = useState("translation will appear here..");
+
+  function changeHandler(event) {
+    const inputEmoji = event.target.value;
+    setEmoji(inputEmoji);
+
+    if (inputEmoji in emojiDictionary) {
+      setMeaning(emojiDictionary[inputEmoji]);
+    } else {
+      setMeaning("failure to recognise this emoji");
+    }
+  }
+
+  function emojiClickHandler(inputEmoji) {
+    setMeaning(emojiDictionary[inputEmoji]);
+  }
+
+  return (
+    <div className="App">
+      <h1>Which Flag is this??</h1>
+      <input
+        onChange={changeHandler}
+        value={emoji}
+        placeholder={"Search your emoji"}
+        style={{
+          padding: "1em",
+          minWidth: "80%"
+        }}
+      />
+      <h2> {emoji} </h2>
+      <h3> {meaning} </h3>
+      {emojis.map((emoji) => (
+        <span
+          onClick={() => emojiClickHandler(emoji)}
+          style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+        >
+          {" "}
+          {emoji}{" "}
+        </span>
+      ))}
+    </div>
+  );
+}
